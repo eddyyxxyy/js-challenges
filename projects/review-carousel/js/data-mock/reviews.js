f4d3775b10cf336f8c06f72d5f6e7a99d3777011
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-09-08 17:47:03 -0300

    chore: adds data-mock for reviews

---
+++ projects/review-carousel/js/data-mock/reviews.js
@@ -0,0 +1,66 @@
+export default [
+  {
+    id: 1,
+    avatar: "https://picsum.photos/id/1/600",
+    name: "Edward Mills",
+    job: "Web Developer",
+    review:
+      "Nam tempus nunc eu dui egestas, sit amet vehicula neque feugiat. Maecenas non aliquet est. Duis diam felis, imperdiet non turpis et, pellentesque efficitur urna. Aliquam turpis tellus, congue vitae ipsum quis, porttitor facilisis metus.",
+  },
+  {
+    id: 2,
+    avatar: "https://picsum.photos/id/2/600",
+    name: "Isabela Gavillan",
+    job: "Web Designer",
+    review:
+      "Curabitur congue consectetur egestas. Suspendisse potenti. Aenean ornare nisi ex, a semper odio tristique sed. Nunc nibh odio, accumsan eget nisi id, viverra dictum urna. Sed tortor mi, convallis efficitur dui nec, rhoncus hendrerit mauris. In euismod consequat dapibus. Donec dui dolor, vulputate at neque sed, sollicitudin dictum risus. In ullamcorper mauris sollicitudin risus iaculis tincidunt. Suspendisse condimentum risus neque, nec bibendum arcu dictum quis. Praesent vulputate purus eget neque dapibus vestibulum. Curabitur suscipit nec ligula eget ullamcorper. Fusce quis libero vehicula velit dignissim ullamcorper. In sagittis vehicula scelerisque. Suspendisse eleifend auctor arcu eget luctus.",
+  },
+  {
+    id: 3,
+    avatar: "https://picsum.photos/id/3/600",
+    name: "Silvana Miranda",
+    job: "Singer/Artist",
+    review:
+      "Etiam pharetra ac neque vitae iaculis. Curabitur in vulputate arcu. Quisque quis dui ligula. Sed aliquam urna nec ipsum pharetra, id tincidunt lectus lobortis.",
+  },
+  {
+    id: 4,
+    avatar: "https://picsum.photos/id/4/600",
+    name: "Francis Bruno",
+    job: "Engineer",
+    review:
+      "Nullam consectetur ligula eget ipsum cursus, at viverra sapien scelerisque. Phasellus consectetur nibh sit amet ligula pharetra, posuere rhoncus magna varius. Phasellus non nulla euismod, sodales nunc consectetur, mollis velit. Morbi condimentum erat eu lectus auctor, at viverra eros mattis.",
+  },
+  {
+    id: 5,
+    avatar: "https://picsum.photos/id/5/600",
+    name: "José Roberto",
+    job: "Database Administrator",
+    review:
+      "Nam tempus nunc eu dui egestas, sit amet vehicula neque feugiat. Maecenas non aliquet est. Duis diam felis, imperdiet non turpis et, pellentesque efficitur urna. Aliquam turpis tellus, congue vitae ipsum quis, porttitor facilisis metus.",
+  },
+  {
+    id: 6,
+    avatar: "https://picsum.photos/id/6/600",
+    name: "Edson Pimenta",
+    job: "Software Engineer",
+    review:
+      "Nullam consectetur ligula eget ipsum cursus, at viverra sapien scelerisque. Phasellus consectetur nibh sit amet ligula pharetra, posuere rhoncus magna varius; Phasellus non nulla euismod.",
+  },
+  {
+    id: 7,
+    avatar: "https://picsum.photos/id/7/600",
+    name: "Lucas Rivoiro",
+    job: "Software Architect",
+    review:
+      "Vestibulum auctor nibh ac tortor luctus, in malesuada nunc eleifend. Sed posuere ante ex, non ultrices sapien facilisis et. Donec nec est sed magna fermentum lobortis et eu dolor. Sed ornare lobortis risus, a vehicula odio luctus nec.",
+  },
+  {
+    id: 8,
+    avatar: "https://picsum.photos/id/8/600",
+    name: "Matheus Barbosa",
+    job: "Copywriter",
+    review:
+      "Duis feugiat tempus lectus. Donec gravida urna sit amet diam elementum pellentesque. Fusce ac turpis vel nisl ornare malesuada et ut felis. Sed rhoncus ut magna aliquet feugiat.",
+  },
+];
